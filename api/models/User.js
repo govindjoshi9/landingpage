cd01afedbd1be219ddc0bafcf3c6460c520d1413
govind <joshigovind740@gmail.com>
2024-03-23 18:41:58 +0530

    first

---
+++ api/models/User.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+const UserSchema = new Schema({
+  name: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true },
+  mobNo: { type: Number, required: true },
+});
+
+const UserModel = mongoose.model("User", UserSchema);
+module.exports = UserModel;
